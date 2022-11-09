b29350cbc0e95f242e0a8728b005b846df4b2e41
Snowden Wintermute <michael.p.silverman@gmail.com>
2022-11-08 19:24:20 -0500

    more delta determining but client prediction broken again?

--- packages/server/src/sockets/battleRoomGame/createGamePhysicsInterval/createDeltaPacket/createDeltaPacket.ts
+++ packages/server/src/sockets/battleRoomGame/createGamePhysicsInterval/createDeltaPacket/createDeltaPacket.ts
@@ -1,15 +1,27 @@
 import isEqual from "lodash.isequal";
-import { BattleRoomGame, PlayerRole } from "../../../../../../common/dist";
+import { BattleRoomGame, Orb, PlayerRole, Point } from "../../../../../../common/dist";
 
-const orbBodyPropNames = ["position", "inertia", "velocity", "angle", "angularVelocity"];
+const orbBodyPropNames = ["position", "inertia", "velocity", "angle"];
+
+type OrbDeltas = { position?: Point; inertia?: number; velocity?: number; angle?: number; isSelected?: boolean; isGhost?: boolean };
 
 export default function (game: BattleRoomGame, playerRole: PlayerRole) {
   if (!game.netcode.prevGameState) return; // send full data
-  const changedOrbs = {};
-
+  const changedOrbsDeltas: {
+    [orbLabel: string]: OrbDeltas;
+  } = {};
   for (let orbLabel in game.netcode.prevGameState.orbs[playerRole]) {
     const prevOrbState = game.netcode.prevGameState.orbs[playerRole][orbLabel];
     const currOrb = game.orbs[playerRole][orbLabel];
-    if (!isEqual(currOrb.body.position, prevOrbState.body.position)) console.log("different position for " + orbLabel);
+    const orbDeltas: OrbDeltas = {};
+    orbBodyPropNames.forEach((propName) => {
+      // @ts-ignore
+      if (!isEqual(currOrb.body[propName], prevOrbState.body[propName])) orbDeltas[propName] = currOrb.body[propName];
+    });
+    if (currOrb.isGhost !== prevOrbState.isGhost) orbDeltas.isGhost = currOrb.isGhost;
+    if (currOrb.isSelected !== prevOrbState.isSelected) orbDeltas.isSelected = currOrb.isSelected;
+
+    if (Object.keys(orbDeltas).length) changedOrbsDeltas[orbLabel] = orbDeltas;
   }
+  if (Object.keys(changedOrbsDeltas).length) console.log(changedOrbsDeltas);
 }
