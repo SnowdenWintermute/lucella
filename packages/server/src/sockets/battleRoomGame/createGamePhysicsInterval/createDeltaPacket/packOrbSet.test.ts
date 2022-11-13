b13c08c3459cb8e6f65fdeb4100ebba5e569e960
Snowden Wintermute <michael.p.silverman@gmail.com>
2022-11-13 08:34:27 -0500

    put tests back in

---
+++ packages/server/src/sockets/battleRoomGame/createGamePhysicsInterval/createDeltaPacket/packOrbSet.test.ts
@@ -0,0 +1,34 @@
+import { createTestGameWithPrevGameState } from "./determineOrbDeltas.test";
+import { OrbsProto, PlayerRole } from "../../../../../../common";
+import determineOrbDeltas from "./determineOrbDeltas";
+import packOrbSet from "./packOrbSet";
+
+test("pack and unpack orb deltas and be able to know if a field was not set", () => {
+  const game = createTestGameWithPrevGameState();
+  game.netcode.prevGameState!.orbs.challenger["challenger-orb-0"].isGhost = true;
+  game.netcode.prevGameState!.orbs.challenger["challenger-orb-1"].isGhost = true;
+  game.netcode.prevGameState!.orbs.challenger["challenger-orb-2"].isSelected = true;
+  const playerOrbDeltas = determineOrbDeltas(game, PlayerRole.CHALLENGER);
+  let packedOrbs;
+  if (playerOrbDeltas && Object.keys(playerOrbDeltas).length) packedOrbs = packOrbSet(playerOrbDeltas);
+  const serializedMessage = packedOrbs?.serializeBinary();
+  const deserializedMessage = OrbsProto.deserializeBinary(serializedMessage!);
+  const orbList = deserializedMessage.getOrbsList();
+  const deserializedObject = JSON.parse(JSON.stringify(deserializedMessage.toObject()));
+  expect(orbList[0].getId()).toBe(0);
+  expect(orbList[1].getId()).toBe(1);
+  expect(orbList[1].hasIsselected()).toBe(false);
+  expect(orbList[1].hasIsghost()).toBe(true);
+  expect(orbList[1].getIsghost()).toBe(false);
+  expect(orbList[2].getId()).toBe(2);
+  expect(orbList[2].hasIsselected()).toBe(true);
+  expect(orbList[2].hasIsghost()).toBe(false);
+  expect(orbList[2].getIsselected()).toBe(false);
+  expect(deserializedObject).toStrictEqual({
+    orbsList: [
+      { id: 0, isselected: false, isghost: false },
+      { id: 1, isselected: false, isghost: false },
+      { id: 2, isselected: false, isghost: false },
+    ],
+  });
+});
