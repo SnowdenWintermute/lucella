5d5a92a6550c79eae13381932ff5caf87f4e3ffb
Snowden Wintermute <michael.p.silverman@gmail.com>
2023-01-03 22:48:14 -0500

    cleaned up .gitignores

--- packages/server/environment.d.ts
+++ packages/server/environment.d.ts
@@ -1,8 +1,39 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      MONGODB_USERNAME: string;
-      MONGODB_PASSWORD: string;
+      EMAIL_PASSWORD: string;
+      SENDGRID_API_KEY: string;
+      DATABASE_URL: string;
+
+      NODE_ENV: string;
+      EMAIL_ROOT_URL: string;
+      EMAIL_ROOT_URL_DEV: string;
+      PORT: number;
+      ORIGIN: string;
+      POSTGRES_USER: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_DB: string;
+
+      REDIS_URL: string;
+
+      ACCESS_TOKEN_EXPIRES_IN: number;
+      AUTH_SESSION_EXPIRATION: number;
+      PASSWORD_RESET_TOKEN_EXPIRES_IN: number;
+      ACCOUNT_ACTIVATION_SESSION_EXPIRATION: number;
+
+      TESTER_KEY: string;
+      CYPRESS_TEST_USER_NAME: string;
+
+      ACCESS_TOKEN_PRIVATE_KEY: string;
+      ACCESS_TOKEN_PUBLIC_KEY: string;
+
+      REFRESH_TOKEN_PRIVATE_KEY: string;
+      REFRESH_TOKEN_PUBLIC_KEY: string;
+
+      PASSWORD_RESET_TOKEN_PRIVATE_KEY: string;
+      PASSWORD_RESET_TOKEN_PUBLIC_KEY: string;
+      ACCOUNT_ACTIVATION_TOKEN_PRIVATE_KEY: string;
+      ACCOUNT_ACTIVATION_TOKEN_PUBLIC_KEY: string;
     }
   }
 }
