f26a167329c5f907572d409ce5f37ec18f12ca52
Snowden Wintermute <michael.p.silverman@gmail.com>
2023-01-20 15:58:04 -0500

    reset error handler

--- packages/server/src/middleware/errorHandler.ts
+++ packages/server/src/middleware/errorHandler.ts
@@ -6,17 +6,18 @@ import CustomError from "../classes/CustomError";
 export default function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
   let status;
   let errors: CustomErrorDetails[] | undefined;
-  return res.status(status || error.status || 500).json(JSON.stringify(error));
-  // if (error[0] instanceof CustomError) {
-  //   status = error[0].status;
-  //   errors = error.map((customError: CustomError) => {
-  //     const errorToReturn: CustomErrorDetails = { message: customError.message };
-  //     if (customError.field) errorToReturn.field = customError.field;
-  //     return errorToReturn;
-  //   });
-  // } else console.error("non-custom error in handler: ", error);
+  if (error[0] instanceof CustomError) {
+    status = error[0].status;
+    errors = error.map((customError: CustomError) => {
+      const errorToReturn: CustomErrorDetails = { message: customError.message };
+      if (customError.field) errorToReturn.field = customError.field;
+      return errorToReturn;
+    });
+  } else console.error("non-custom error in handler: ", error);
 
-  // let jsonToSend;
-  // if (errors) jsonToSend = errors;
-  // else jsonToSend = [{ message: ErrorMessages.SERVER_GENERIC }];
+  let jsonToSend;
+  if (errors) jsonToSend = errors;
+  else jsonToSend = [{ message: ErrorMessages.SERVER_GENERIC }];
+
+  res.status(status || error.status || 500).json(jsonToSend);
 }
