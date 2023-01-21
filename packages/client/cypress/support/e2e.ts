178bdf1f16d4223daee520f07e17b8833c2a1c15
Snowden Wintermute <michael.p.silverman@gmail.com>
2023-01-21 09:09:10 -0500

    cypress log uncaught exceptions instead of failing

--- packages/client/cypress/support/e2e.ts
+++ packages/client/cypress/support/e2e.ts
@@ -18,3 +18,9 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from failing the test
+  console.log("cypress uncaught exception: ", err);
+  return false;
+});
