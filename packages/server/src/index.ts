61f1990914fb975a95b430cb48ac9bd0ed396f51
Snowden Wintermute <michael.p.silverman@gmail.com>
2023-02-10 18:46:30 -0500

    toxiproxy added to dev docker-compose

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,14 +3,12 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import { DatabaseError } from "pg";
-import { Latency } from "toxiproxy-node-client";
 import { LucellaServer } from "./classes/LucellaServer";
 import { pgOptions } from "./database/config";
 import createExpressApp from "./createExpressApp";
 import wrappedPool from "./database/wrappedPool";
 import { wrappedRedis, RedisContext } from "./utils/RedisContext";
 import { lucella } from "./lucella";
-import { getToxic } from "./toxiproxy";
 
 const { PORT } = process.env;
 
@@ -23,9 +21,6 @@ wrappedPool
       await wrappedRedis.context.connect();
       console.log(`express server on port ${PORT}`);
       lucella.server = new LucellaServer(listening);
-      // getToxic("latency", <Latency>{ latency: 400, jitter: 100, toxicity: 1, stream: "downstream" })
-      //   .then((toxic) => console.log(toxic.toJson()))
-      //   .catch(console.error);
     });
   })
   .catch((error: DatabaseError) => console.error(error));
