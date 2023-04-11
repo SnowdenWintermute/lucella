d77e24ae4133d144ac1caf60fcdaa3196e98f05b
Snowden Wintermute <michael.p.silverman@gmail.com>
2023-04-11 18:55:42 -0400

    patch notes button and started logged in user main menu screen

--- packages/client/src/components/Lobby/LobbyMenus/MainMenu.tsx
+++ packages/client/src/components/Lobby/LobbyMenus/MainMenu.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Socket } from "socket.io-client";
 import Link from "next/link";
 import MainMenuButtons from "./MainMenuButtons";
 import Logo from "../../../img/logo.svg";
+import { useGetMeQuery } from "../../../redux/api-slices/users-api-slice";
+import { useGetLadderEntryQuery } from "../../../redux/api-slices/ladder-api-slice";
+import LoadingSpinner from "../../common-components/LoadingSpinner";
 
 function WelcomeDropdown() {
-  return (
-    <section className="lobby-menu welcome-menu">
-      <div className="welcome-menu__copy-with-links">
+  const { data: user, isLoading: userIsLoading } = useGetMeQuery(null);
+  const {
+    isLoading: userScorecardIsLoading,
+    isFetching: userScorecardIsFetching,
+    data: userScorecardData,
+    error: userScorecardError,
+    refetch: userScorecardRefetch,
+  } = useGetLadderEntryQuery(user?.name || "", { refetchOnMountOrArgChange: true });
+  const [viewingPatchNotes, setViewingPatchNotes] = useState(false);
+
+  let mainMenuLeftContent = <LoadingSpinner />;
+
+  if (userIsLoading) mainMenuLeftContent = <LoadingSpinner />;
+  else if (!viewingPatchNotes && !user)
+    mainMenuLeftContent = (
+      <>
         <p>Welcome to Battle School, a space station orbiting Earth at the 5th Lagrange Point.</p>
         <p>Create an account to have your name listed on the ladder, or play a casual game to learn the rules before entering the competition.</p>
         <div className="welcome-menu__links">
@@ -18,9 +34,58 @@ function WelcomeDropdown() {
             Create Account
           </Link>
         </div>
+      </>
+    );
+  else if (!viewingPatchNotes && user)
+    mainMenuLeftContent = (
+      <div>
+        <h4>Logged in as: {user.name}</h4>
+        {userScorecardData && (
+          <ul>
+            <li>Rank: {userScorecardData.ladderEntry.rank}</li>
+            <li>Elo: {userScorecardData.ladderEntry.elo}</li>
+            <li>Wins: {userScorecardData.ladderEntry.wins}</li>
+            <li>Losses: {userScorecardData.ladderEntry.losses}</li>
+          </ul>
+        )}
+        {/* @ts-ignore */}
+        {userScorecardError && <p>{userScorecardError.data[0].message}</p>}
       </div>
-      <Logo className="welcome-menu__logo" />
-      <span className="welcome-menu__version">alpha 0.2.0</span>
+    );
+  else if (viewingPatchNotes)
+    mainMenuLeftContent = (
+      <div className="patch-notes">
+        <div>
+          <h4>alpha 0.2.0 patch notes</h4>
+        </div>
+        <ul>
+          <li>Major overhaul of frontend styles</li>
+          {/* eslint-disable-next-line react/jsx-curly-brace-presence */}
+          <li>{'New themes, "VT320" and "HTML", currently accessible by clicking the website logo in the navbar'}</li>
+          <li>Optimized modals and context menus</li>
+          <li>Added support for screenreaders</li>
+          <li>Fixed tab indexing of UI elements for better keyboard navigation</li>
+          <li>Added loading spinners and indicatiors for several menus</li>
+          <li>Changed to a new logo</li>
+          <li>Updated the Battle Room colors to match the website themes (VT320 and default themes only)</li>
+          <li>Added ability to close lobby menus with the Escape key</li>
+          <li>Links can now be posted in chat</li>
+        </ul>
+        <button type="button" className="button" onClick={() => setViewingPatchNotes(false)}>
+          Close
+        </button>
+      </div>
+    );
+
+  return (
+    <section className="lobby-menu welcome-menu">
+      <div className="lobby-menu__left welcome-menu__left">{mainMenuLeftContent}</div>
+      <div className="lobby-menu__right welcome-menu__right">
+        <Logo className="welcome-menu__logo" />
+        <button type="button" className="welcome-menu__version" onClick={() => setViewingPatchNotes(!viewingPatchNotes)}>
+          alpha 0.2.0
+        </button>
+      </div>
     </section>
   );
 }
