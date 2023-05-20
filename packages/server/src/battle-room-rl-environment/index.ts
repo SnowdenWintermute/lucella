99194e3e8ff4e97dfa5a104c31d260cf817929e0
Mike <michael.p.silverman@gmail.com>
2023-05-20 16:13:36 -0400

    translated action space formatted actions to UserInput instances on the JS side

--- packages/server/src/battle-room-rl-environment/index.ts
+++ packages/server/src/battle-room-rl-environment/index.ts
@@ -1,11 +1,24 @@
+/* eslint-disable camelcase */
 import Matter, { Detector } from "matter-js";
-import { baseSpeedModifier, BattleRoomGame, processPlayerInput, renderRate, setOrbsAtStartPositions, UserInput } from "../../../common";
+import {
+  baseSpeedModifier,
+  BattleRoomGame,
+  ClientTickNumber,
+  PlayerRole,
+  Point,
+  processPlayerInput,
+  renderRate,
+  SelectOrbAndAssignDestination,
+  setOrbsAtStartPositions,
+  UserInput,
+} from "../../../common";
 import resetScoreAndSpeed from "../battleRoomGame/createGamePhysicsInterval/handleScoringPoints/resetScoreAndSpeed";
 import updateScoreNeededToWin from "../battleRoomGame/createGamePhysicsInterval/handleScoringPoints/updateScoreNeededToWin";
 
 class CustomEnvironment {
   possible_agents = ["host", "challenger"];
   agents = ["host", "challenger"];
+  inputNumber = 0;
   MAX_TIMESTEP = 6000;
   timestep = 0;
   game = new BattleRoomGame("game-name", { host: this.possible_agents[0], challenger: this.possible_agents[1] });
@@ -24,42 +37,50 @@ class CustomEnvironment {
     return [this.getObservations(), {}];
   }
 
-  step() {
-    const rewards = { host: 0, challenger: 0 };
-    let terminated = false;
-    let truncated = false;
+  step(actions: { playerRole: PlayerRole; cursor_position: number[]; number_key_pressed: number }[]) {
+    Object.values(actions).forEach((action) => {
+      const [x, y] = action.cursor_position;
+      const { number_key_pressed, playerRole } = action;
+      const orbIds = [];
+      if (number_key_pressed) orbIds.push(number_key_pressed);
+      this.inputNumber += 1;
+      this.game.queues.server.receivedInputs.push(new SelectOrbAndAssignDestination({ orbIds, mousePosition: new Point(x, y) }, this.inputNumber, playerRole));
+      this.inputNumber += 1;
+      this.game.queues.server.receivedInputs.push(new ClientTickNumber(null, this.inputNumber, playerRole));
+    });
 
-    const { game } = this;
-    let numInputsToProcess = game.queues.server.receivedInputs.length;
-    while (numInputsToProcess > 0) {
-      const input: UserInput = game.queues.server.receivedInputs.shift();
-      processPlayerInput(input, game, renderRate, input.playerRole);
-      numInputsToProcess -= 1;
-      const collisions = Detector.collisions(game.physicsEngine!.detector);
-      collisions.forEach((collision) => {
-        game.currentCollisionPairs.push(Matter.Pair.create(collision, +Date.now()));
-      });
-    }
-    updateScoreNeededToWin(game);
-
-    // handle scoring points and calculate rewards / terminated / truncated
-    const hostNewPoints = this.game.score.host - this.scoresFromPreviousStep.host;
-    if (hostNewPoints > 0) rewards.host += hostNewPoints;
-    const challengerNewPoints = this.game.score.challenger - this.scoresFromPreviousStep.challenger;
-    if (challengerNewPoints > 0) rewards.challenger += challengerNewPoints;
-    const challengerWon = game.score.challenger >= game.score.neededToWin;
-    const hostWon = game.score.host >= game.score.neededToWin;
-    if (hostWon) rewards.host += 10;
-    if (challengerWon) rewards.challenger += 10;
-    if (hostWon || challengerWon) terminated = true;
-    if (!terminated && this.timestep > this.MAX_TIMESTEP) truncated = true;
-    this.timestep += 1;
-
-    const terminations = { host: terminated, challenger: terminated };
-    const truncations = { host: truncated, challenger: truncated };
-    const info = {};
-
-    return [this.getObservations(), rewards, terminations, truncations, info];
+    console.log(JSON.stringify(this.game.queues.server.receivedInputs, null, 2));
+    // const rewards = { host: 0, challenger: 0 };
+    // let terminated = false;
+    // let truncated = false;
+    // const { game } = this;
+    // let numInputsToProcess = game.queues.server.receivedInputs.length;
+    // while (numInputsToProcess > 0) {
+    //   const input: UserInput = game.queues.server.receivedInputs.shift();
+    //   processPlayerInput(input, game, renderRate, input.playerRole);
+    //   numInputsToProcess -= 1;
+    //   const collisions = Detector.collisions(game.physicsEngine!.detector);
+    //   collisions.forEach((collision) => {
+    //     game.currentCollisionPairs.push(Matter.Pair.create(collision, +Date.now()));
+    //   });
+    // }
+    // updateScoreNeededToWin(game);
+    // // handle scoring points and calculate rewards / terminated / truncated
+    // const hostNewPoints = this.game.score.host - this.scoresFromPreviousStep.host;
+    // if (hostNewPoints > 0) rewards.host += hostNewPoints;
+    // const challengerNewPoints = this.game.score.challenger - this.scoresFromPreviousStep.challenger;
+    // if (challengerNewPoints > 0) rewards.challenger += challengerNewPoints;
+    // const challengerWon = game.score.challenger >= game.score.neededToWin;
+    // const hostWon = game.score.host >= game.score.neededToWin;
+    // if (hostWon) rewards.host += 10;
+    // if (challengerWon) rewards.challenger += 10;
+    // if (hostWon || challengerWon) terminated = true;
+    // if (!terminated && this.timestep > this.MAX_TIMESTEP) truncated = true;
+    // this.timestep += 1;
+    // const terminations = { host: terminated, challenger: terminated };
+    // const truncations = { host: truncated, challenger: truncated };
+    // const info = {};
+    // return [this.getObservations(), rewards, terminations, truncations, info];
   }
 
   getObservations() {
